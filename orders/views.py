bccd11bbe0ab858869a5903d0f5ab0cd0119d3fe
Angel Garcia Frias <angelgarciafrias@Angels-MBP.home>
2020-06-11 11:52:48 +0200

    Users implemented

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,43 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message": None})
+    context = {"user": request.user}
+    return render(request, "orders/index.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def register(request):
+
+    form = UserCreationForm()
+    if request.method == "POST":
+        
+        form = UserCreationForm(data=request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+
+            if user is not None:
+                return HttpResponseRedirect(reverse("index"))
+
+    form.fields['username'].help_text = None
+    form.fields['password1'].help_text = None
+    form.fields['password2'].help_text = None
+    return render(request, "orders/register.html", {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
