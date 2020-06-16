b84a1e845fb80fd92276c021376cbc31b410b993
Angel Garcia Frias <angelgarciafrias@Angels-MBP.home>
2020-06-16 13:07:28 +0200

    Database implemented

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Flavors, Crusts, Sizes, Extras, Pizzas
 
-# Register your models here.
+class ExtraInline(admin.StackedInline):
+    model = Extras.pizzas_extra.through
+    extra = 1
+
+class PizzasAdmin(admin.ModelAdmin):
+    inlines = [ExtraInline]
+
+class ExtrasAdmin(admin.ModelAdmin):
+    filter_horizontal = ("pizzas_extra",)
+
+admin.site.register(Flavors)
+admin.site.register(Crusts)
+admin.site.register(Sizes)
+admin.site.register(Extras, ExtrasAdmin)
+admin.site.register(Pizzas, PizzasAdmin)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Flavors(models.Model):
+    typeof_flavor = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.typeof_flavor}"
+
+class Crusts(models.Model):
+    typeof_crust = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.typeof_crust}"
+
+class Sizes(models.Model):
+    typeof_size = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.typeof_size}"
+
+class Pizzas(models.Model):
+    pizza_flavor = models.ForeignKey(Flavors, on_delete=models.CASCADE, related_name="pizzas_flavor")
+    pizza_crust = models.ForeignKey(Crusts, on_delete=models.CASCADE, related_name="pizzas_crust")
+    pizza_size = models.ForeignKey(Sizes, on_delete=models.CASCADE, related_name="pizzas_size")
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"Pizza: {self.pizza_flavor}, {self.pizza_crust}, {self.pizza_size}"
+
+class Extras(models.Model):
+    typeof_extra = models.CharField(max_length=64, blank=True, default='')
+    pizzas_extra = models.ManyToManyField(Pizzas, blank=True, related_name="pizzas_extra")
+
+    def __str__(self):
+        return f"{self.typeof_extra}"
